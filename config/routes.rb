524e78f50c96f059d91cf941d25eea36c4ebc880
duranus <bjryj244@yahoo.co.jp>
2020-10-12 23:15:17 +0900

    20201012test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#new'
+  root 'users#login_form'
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
